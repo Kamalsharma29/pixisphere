30ec70584e562d12c051ba31ac266c68a777bce2
Kamal Kumar Sharma <109137285+Kamalsharma29@users.noreply.github.com>
2025-07-08 13:59:11 +0530

    Fix ESLint config for Vercel build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  root: true,
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@next/next/no-img-element': 'off',
   },
 };
